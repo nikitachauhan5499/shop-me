c4e6183c414153a98f713952a9f067c68335981b
Nikita Chauhan <nikitachauhan5499@gmail.com>
2021-09-11 00:24:48 +0530

    node server setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to node server</h1>');
+});
+
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+
+app.get('/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
+});
+
+app.listen(8080, () => { console.log('Server listening to port 8080')});
