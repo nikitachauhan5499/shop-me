0d1b3b4b146f7e4f8e44fbb58a50c7a0184497a5
Nikita Chauhan <nikitachauhan5499@gmail.com>
2021-09-22 08:31:28 +0530

    Create products list reducer and action

---
+++ frontend/src/reducers/productReducer.js
@@ -0,0 +1,10 @@
+import { PRODUCT_LIST_FAILS, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST } from '../constants/productConstant';
+
+export const productListReducer = (state = {products: []}, action) => {
+    switch(action.type) {
+        case PRODUCT_LIST_REQUEST: return { loading: true, products: [] };
+        case PRODUCT_LIST_SUCCESS: return { loading: false, products: action.payload };
+        case PRODUCT_LIST_FAILS: return { loading: false, error: action.payload };
+        default: return state;
+    }
+} 
