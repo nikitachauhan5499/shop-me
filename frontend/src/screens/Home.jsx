a2ee3ea956e170fcc219176c029fa564811cef03
Nikita Chauhan <nikitachauhan5499@gmail.com>
2021-09-22 08:45:29 +0530

    Display products using redux

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from './Product';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 const Home = () => {
-    const [ products, setProducts ] = useState([]);
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get("/api/products");
-            setProducts(data);
-        }
-        fetchProducts();
-    }, [])
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products } = productList;
 
+    useEffect(() => {
+        dispatch(listProducts());
+    }, [dispatch]);
     return (
-        <Row>
+        <div>
+        {
+            loading ? <h2>Loading...</h2> : error ? <h2> { error } </h2> : <Row>
             {
                 products.map(product => {
                     return <Col key={product._id} md={3}>
@@ -23,7 +24,9 @@ const Home = () => {
                 })
             }  
         </Row>  
+        }
         
+        </div>
     )
 }
 
